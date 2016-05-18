7397c1c8a82891af3e6a7c57c6f44d9c3490f924
Carla04 <carla04@yahoo.com>
2016-05-18 13:40:33 +0200

    Add files via upload

---
+++ makeCacheMatrix22.R
@@ -0,0 +1,14 @@
+makeCacheMatrix22<- function(x =data.matrix()){
+        
+m <- NULL
+set <- function(y){
+        x<<-y
+        m<<-NULL
+}
+        
+get<-function() x
+setinversematrix<- function(solve) m<<- solve
+getinversematrix<- function() m
+
+list(set=set,get=get, setinversematrix=setinversematrix,getinversematrix=getinversematrix)
+}
